5d15464198906317c3d19a608865002dabb601f4
Rebecca S <sithel@gmail.com>
2013-04-26 20:36:40 -0700

    Base App Engine Project
    Here's the almost-nothing rigging for the App Engine server, as yanked
    out of my Eclipse workspace

---
+++ giftid/src/paper/yarn/ink/GiftidServlet.java
@@ -0,0 +1,13 @@
+package paper.yarn.ink;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class GiftidServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
